3730df9fd4201b8557794bbf9a65b205475e20e2
Frank Kloeker <f.kloeker@telekom.de>
2021-09-09 13:11:41 +0200

    bump tf versions

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     opentelekomcloud = {
       source  = "opentelekomcloud/opentelekomcloud"
-      version = ">=1.25.3-SNAPSHOT-09496217"
+      version = ">=1.25.4"
     }
     template = {
       source = "hashicorp/template"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
 }
