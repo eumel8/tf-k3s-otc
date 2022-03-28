b5dea757b033262cba95d21005319b508d26f5f4
Frank Kloeker <f.kloeker@telekom.de>
2022-03-28 16:17:34 +0200

    bump provider version
    https://github.com/opentelekomcloud/terraform-provider-opentelekomcloud/issues/1671

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     opentelekomcloud = {
       source  = "opentelekomcloud/opentelekomcloud"
-      version = ">=1.25.4"
+      version = ">=1.28.2"
     }
   }
   required_version = ">= 1.1.0"
